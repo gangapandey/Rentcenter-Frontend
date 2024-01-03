6c6320c2155c640c7a32c908251d2b31779c4ff1
gangapandey <igangapandey@gmail.com>
2024-01-03 15:35:25 +0545

    product filtering

--- src/pages/product/productAPI.js
+++ src/pages/product/productAPI.js
@@ -6,3 +6,28 @@ export function fetchAllProducts(){
         resolve({data})
     })
 }
+export function fetchProductsByFilters(filter, sort){
+    //filter = {"category" : "smartphones", "Laptops"}
+    // sort ={_sort: "price", _order="desc"}
+    //TODO: on server support multiple category
+    let queryString = '';
+    for( let key in filter){
+        const categoryValues = filter[key];
+        if(categoryValues.length){
+        //     const lastCategoryValues = categoryValues[categoryValues.length-1]
+        // queryString += `${key}=${lastCategoryValues}&`
+        const lastCategoryValues = categoryValues[categoryValues.length-1]
+        queryString += `${key}=${lastCategoryValues}&`
+        } 
+    }
+    for(let key in sort){
+        queryString += `${key}=${sort[key]}&`
+    }
+
+    return new Promise(async(resolve)=>{
+        //todo
+        const response = await fetch("http://localhost:3000/products?"+queryString)
+        const data = await response.json()
+        resolve({data})
+    })
+}
--- src/pages/product/productSlice.js
+++ src/pages/product/productSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { fetchAllProducts } from './productAPI';
+import { fetchAllProducts, fetchProductsByFilters } from './productAPI';
 
 const initialState = {
     products: [],
@@ -10,6 +10,14 @@ export const fetchAllProductsAsync = createAsyncThunk(
     'product/fetchAllProducts',
     async () => {
         const response = await fetchAllProducts();
+        return response.data;
+    }
+);
+
+export const fetchProductsByFiltersAsync = createAsyncThunk(
+    'product/fetchProductsByFilters',
+    async ({filter, sort}) => {
+        const response = await fetchProductsByFilters(filter, sort);
         return response.data;
     }
 );
@@ -28,6 +36,13 @@ export const productSlice = createSlice({
                 state.status = 'loading';
             })
             .addCase(fetchAllProductsAsync.fulfilled, (state, action) => {
+                state.status = 'idle';
+                state.products = action.payload;
+            })
+            .addCase(fetchProductsByFiltersAsync.pending, (state) => {
+                state.status = 'loading';
+            })
+            .addCase(fetchProductsByFiltersAsync.fulfilled, (state, action) => {
                 state.status = 'idle';
                 state.products = action.payload;
             });
